fd021dc65fa06b4eeeb5f26be4f0e5dd609fec89
ezra buchla <emb@catfact.net>
2019-06-03 23:18:44 -0400

    (wip) cleanup, fix brace style etc

--- softcut~.cpp
+++ softcut~.cpp
@@ -13,23 +13,21 @@
 #include "ext_buffer.h"
 
 // softcut includes
+#include "src/SoftCutVoice.h"
 
-class Test {
-    float a;
-    float b;
-    Test() {
-        a = 1.f;
-        b = 10.f;
-    }
-    ~Test() {}
-};
+#define SOFTCUT_IO_BUF_FRAMES 8192
 
 typedef struct _softcut {
 	t_pxobject l_obj;
 	t_buffer_ref *l_buffer_reference;
-	long l_chan;
-} t_softcut;
 
+    softcut::SoftCutVoice scv;
+    
+    // FIXME: this is dumb, but softcut uses floats not doubles,
+    // instead of changing it we'll just maintain internal I/O buffers and convert
+    float inBuf[SOFTCUT_IO_BUF_FRAMES];
+    float outBuf[SOFTCUT_IO_BUF_FRAMES];
+} t_softcut;
 
 void softcut_perform64(t_softcut *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
 void softcut_dsp64(t_softcut *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
@@ -37,7 +35,6 @@ void softcut_set(t_softcut *x, t_symbol *s);
 void *softcut_new(t_symbol *s, long chan);
 void softcut_free(t_softcut *x);
 t_max_err softcut_notify(t_softcut *x, t_symbol *s, t_symbol *msg, void *sender, void *data);
-void softcut_in1(t_softcut *x, long n);
 void softcut_assist(t_softcut *x, void *b, long m, long a, char *s);
 void softcut_dblclick(t_softcut *x);
 
@@ -51,7 +48,6 @@ void ext_main(void *r)
 
 	class_addmethod(c, (method)softcut_dsp64, "dsp64", A_CANT, 0);
 	class_addmethod(c, (method)softcut_set, "set", A_SYM, 0);
-	class_addmethod(c, (method)softcut_in1, "in1", A_LONG, 0);
 	class_addmethod(c, (method)softcut_assist, "assist", A_CANT, 0);
 	class_addmethod(c, (method)softcut_dblclick, "dblclick", A_CANT, 0);
 	class_addmethod(c, (method)softcut_notify, "notify", A_CANT, 0);
@@ -67,59 +63,43 @@ void softcut_perform64(t_softcut *x, t_object *dsp64, double **ins, long numins,
 	t_double	*out = outs[0];
 	int			n = sampleframes;
 	t_float		*tab;
-	double		temp;
-	double		f;
-	long		softcut, chan, frames, nc;
 	t_buffer_obj	*buffer = buffer_ref_getobject(x->l_buffer_reference);
 
 	tab = buffer_locksamples(buffer);
-	if (!tab)
-		goto zero;
-
-	frames = buffer_getframecount(buffer);
-	nc = buffer_getchannelcount(buffer);
-	chan = MIN(x->l_chan, nc);
-	while (n--) {
-		temp = *in++;
-		f = temp + 0.5;
-		softcut = f;
-		if (softcut < 0)
-			softcut = 0;
-		else if (softcut >= frames)
-			softcut = frames - 1;
-		if (nc > 1)
-			softcut = softcut * nc + chan;
-		*out++ = tab[softcut];
-	}
+    if (!tab) {
+        goto zero;
+    }
+    // FIXME? assuming buffer is mono.
+    // dunno if multichannel MSP bufs are interleaved or not.
+    // if they are, this will be messed up and it's probably best to have a channel selection
+    x->scv.setBuffer(tab, buffer_getframecount(buffer));
+    
+    for (int i=0; i<n; ++i) { x->inBuf[i] = (float)(*in++); }
+    x->scv.processBlockMono(x->inBuf, x->outBuf, n);
+    for (int i=0; i<n; ++i) { *out++ = (double)(x->outBuf[i]); }
+    
 	buffer_unlocksamples(buffer);
+
 	return;
 zero:
-	while (n--)
-		*out++ = 0.0;
+    while (n--) { *out++ = 0.0; }
 }
 
 void softcut_set(t_softcut *x, t_symbol *s)
 {
-	if (!x->l_buffer_reference)
+    if (!x->l_buffer_reference) {
 		x->l_buffer_reference = buffer_ref_new((t_object *)x, s);
-	else
+    }
+    else {
 		buffer_ref_set(x->l_buffer_reference, s);
+    }
 }
-
-void softcut_in1(t_softcut *x, long n)
-{
-	if (n)
-		x->l_chan = MAX(n, 1) - 1;
-	else
-		x->l_chan = 0;
-}
-
 
 void softcut_dsp64(t_softcut *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
 {
 	dsp_add64(dsp64, (t_object *)x, (t_perfroutine64)softcut_perform64, 0, NULL);
+    x->scv.setSampleRate(samplerate);
 }
-
 
 // this lets us double-click on softcut~ to open up the buffer~ it references
 void softcut_dblclick(t_softcut *x)
@@ -129,8 +109,9 @@ void softcut_dblclick(t_softcut *x)
 
 void softcut_assist(t_softcut *x, void *b, long m, long a, char *s)
 {
-	if (m == ASSIST_OUTLET)
+    if (m == ASSIST_OUTLET) {
 		sprintf(s,"(signal) Sample Value at softcut");
+    }
 	else {
 		switch (a) {
 		case 0:	sprintf(s,"(signal) Sample softcut");	break;
@@ -146,7 +127,6 @@ void *softcut_new(t_symbol *s, long chan)
 	intin((t_object *)x,1);
 	outlet_new((t_object *)x, "signal");
 	softcut_set(x, s);
-	softcut_in1(x,chan);
 	return (x);
 }
 
